37a7e54968854fa3e4b6e45a67f92f0f4be5bbc0
Cristian Viery <viery.guimaraes01@gmail.com>
2023-04-11 10:56:29 -0300

    Inicio
    site com parte de buscar estoque finalizado

---
+++ arealogin/script.js
@@ -0,0 +1,23 @@
+const users = [
+    {
+        name:"Cristian",
+        password:"viery2312"
+    },
+    {
+        name:"Admin",
+        password:"admin"
+    }
+]
+   function validar() {
+    const userName = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+    for(i of users){
+        const iName = i.name
+        const iPass = i.password
+        if (userName == iName && password == iPass) {
+            alert(`Olá! ${userName}, sejá bem vindo!`);
+            window.location.href = '../home/home.html';
+        }
+    }}
+
+    
